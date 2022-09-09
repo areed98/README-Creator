1145c57985a348a4d07ff78fa5228d8d5401f889
areed98 <austinjreed89@gmail.com>
2022-09-09 12:26:19 -0400

    Mostly done with index.js, started the license onn generateMarkdown.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,55 +1,145 @@
 // Include packages needed for this application
 const inquirer = require("Inquirer");
 const fs = require('fs');
-
+const MarkdownMaker = require('./utils/generateMarkdown.js');
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
 const questions = [
         {
             type: 'input',
-            name: 'Project-Title',
-            message: 'What is the name of your project?'
+            name: 'Title',
+            message: 'What is the name of your project?',
+            validate: Titleinput => {
+                if (Titleinput) {
+                    return true;
+                } else {
+                    console.log('Please enter a title name!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
-            name: 'Project-Description',
-            message: 'Please fill out a description of your project.'
+            name: 'Description',
+            message: 'Please fill out a description of your project.',
+            validate: Descriptioninput => {
+                if (Descriptioninput) {
+                    return true;
+                } else {
+                    console.log('Please enter a description!');
+                    return false;
+                }
+            }
         },
         {
             type: 'list',
             name: 'License',
             message: 'Please input what licenses you may have!',
-            choices: ['MIT', 'ISC', 'GNU GPLv3', 'Apache License 2.0', 'Other', 'No License']
+            choices: ['MIT', 'ISC', 'GPL-3.0', 'Apache-2.0', 'Unilicense', 'No License']
         },
         {
             type: 'input',
             name: 'Installation',
-            message: 'What is necessary to get the project installed?'
+            message: 'What is necessary to get the project installed?',
+            validate: Installationinput => {
+                if (Installationinput) {
+                    return true;
+                } else {
+                    console.log('Please provide installation instructions!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'Usage',
-            message: 'What is the intended use for the project, and what are some examples?'
+            message: 'What is the intended use for the project, and what are some examples?',
+            validate: Usageinput => {
+                if (Usageinput) {
+                    return true;
+                } else {
+                    console.log('Please provide information on what this is used for!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
-            name: 'Credits',
-            message: 'List the collaborators and other assets used to complete the project!'
+            name: 'Contribution',
+            message: 'List the collaborators and other assets used to complete the project!',
+            validate: Contributioninput => {
+                if (Contributioninput) {
+                    return true;
+                } else {
+                    console.log('Please enter a list of contributers! (This includes you!)');
+                    return false;
+                }
+            }
         },
-        // {
-        //     type: 'list',
-        //     name: 'Badges',
-        //     message: 'Please select any badges that you would like to be shown on your project page!',
-        //     choices: ['']
-        // }
-
+        {
+            type: 'input',
+            name: 'Test',
+            message: 'Please provide information on how to test the application!',
+            validate: Testinput => {
+                if (Testinput) {
+                    return true;
+                } else {
+                    console.log('Please provide instructions for testing!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'username',
+            message: 'Please input your GitHub username!',
+            validate: usernameinput => {
+                if (usernameinput) {
+                    return true;
+                } else {
+                    console.log('Please enter a username!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please input your email address!',
+            validate: emailinput => {
+                if (emailinput) {
+                    return true;
+                } else {
+                    console.log('Please enter an email!');
+                    return false;
+                }
+            }
+        }
     ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+    fs.writeFile('Generated-README.md', data, err =>
+    err ? console.log(err) : console.log('README Generated!'));
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then(answers => {
+            return answers;
+        })
+};
+
 
 // Function call to initialize app
-init();
+init()
+.then(answers => {
+    console.log(answers);
+    return generateMarkdown(answers);
+})
+.catch(err => {
+    console.log(err);
+})
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,67 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(License) {
+  if (License == 'No License') {
+    return '';
+  }
+  else {
+    return `
+    ![Badge](https://img.shields.io/badge/license-${License}-blue)
+    `;
+  }
+};
 
-// TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(License) {
+  if (License == 'No License') {
+    return '';
+  }
+  else {
+    return `
+    [${License}](https://choosealicense.com/licenses/${License})
+    `;
+  }
+}
 
-// TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(License) {
+  if (license == 'No License') {
+    return '';
+  }
+  else {
+    return `
+    ## [License](#table-of-contents)
+    
+    The application is covered under the following license(s):
+    
+    ${renderLicenseLink(License)}
+    `;
+  }
+}
+// Create table of contents if statement for license
+function LicenseContents(License) {
+  if (License == 'No License') {
+    return '';
+  }
+  else {
+    return `
+    * [License](#License)
+    `;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
+
+  ## Table-of-Contents
+
+  * [Description](#Description)
+  * [Installation](#Installation)
+  * [Usage](Usage)
+  * [Contribution](Contribution)
+  * [Tests](Tests)
+  * 
 
 `;
 }
